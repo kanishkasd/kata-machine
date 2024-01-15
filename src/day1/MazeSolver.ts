bf6297c3a95223e507dac7f154722d3584128916
kanishkasd <kanishkasandaru2048@gmail.com>
2024-01-15 11:04:12 +0530

    completed the kata-machine

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -11,7 +11,6 @@ function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boole
     // off the map
     if (curr.x < 0 || curr.x >= maze[0].length ||
         curr.y < 0 || curr.y >= maze.length) {
-
         return false;
     }
 
@@ -37,32 +36,28 @@ function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boole
     path.push(curr)
 
     // recurse
-    for(let i = 0; i < dir.length; ++i){
-        const [ x , y ] = dir[i];
-        if(walk(maze, wall, {
+    for (let i = 0; i < dir.length; ++i) {
+        const [x, y] = dir[i];
+        if (walk(maze, wall, {
             x: curr.x + x,
             y: curr.y + y,
-        }, end, seen, path)){
-            return  true;
+        }, end, seen, path)) {
+            return true;
         }
     }
     // post
     path.pop()
-     
     return false;
 }
 
 export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
-
-
     const seen: boolean[][] = [];
     const path: Point[] = [];
 
-    for(let i = 0; i < maze.length; i++){
+    for (let i = 0; i < maze.length; i++) {
         seen.push(new Array(maze[0].length).fill(false));
     }
 
-    walk(maze, wall, start,  end , seen , path  )
-    
-    return path 
+    walk(maze, wall, start, end, seen, path)
+    return path
 }
